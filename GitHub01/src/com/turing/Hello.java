92f420445e5cd4f04d30fca456c18e9e5b26b283
Fred <1521644491@qq.com>
2019-10-25 11:01:44 +0800

    lrt:初始化项目

--- GitHub01/src/com/turing/Hello.java
+++ GitHub01/src/com/turing/Hello.java
@@ -1,7 +1,20 @@
 package com.turing;
 
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello");
+        Scanner s = new Scanner(System.in);
+        System.out.print("请输入");
+        int count = 5;
+        for (int i=0;i<count;i++){
+            String next = s.next();
+            if(next.equals("1")){
+                System.out.print("hello");
+            }else {
+                System.out.print("hello2");
+            }
+        }
+
     }
 }
